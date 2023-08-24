0130dcb6cfba0457267630123c3c4529846a5a79
Mohamoammar <mohamax250@gmail.com>
2023-08-24 15:31:34 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,14 @@
-#ifndef lISTS_H
-#define lISTS_H
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
- * structer  list_s - singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
- * @len: length of string
- * @next: points to next node
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Desc: singly linked list node structure
- * for Holbert project
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -21,9 +21,14 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-void the  free_list(list_t *head);
+void free_list(list_t *head);
 
 #endif
+
+
+
+
+
 
 
 
